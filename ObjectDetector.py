b76e5fc3f5b5b77f500cdc2c6977b0dde5c03b1e
qwackies1 <123024113+qwackies1@users.noreply.github.com>
2023-01-18 16:12:49 -0500

    Add files via upload

---
+++ ObjectDetector.py
@@ -0,0 +1,84 @@
+import cv2
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email():
+    msg = EmailMessage()
+    msg['Subject'] = "Motion Detected 👀"
+    msg['From'] = "Boba Watchdog"
+    msg['To'] = "chaseliu99@gmail.com"
+    msg.set_content("Motion has been detected in the basement. Please check the gate to see if Boba went through.")
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login("bobawatchdog123@gmail.com", "xvlouurkkbmtpoib")
+    server.send_message(msg)
+    server.quit()
+    print('Email has been sent')
+
+
+coco_names = []
+coco_names_file = 'coco.names'
+
+with open(coco_names_file, 'rt') as f:
+    coco_names = f.read().rstrip('\n').split('\n')
+
+config_path = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weights_path = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weights_path, config_path)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+def get_objects(img, draw_box=True, objects=[], threshold=0.5):
+    class_ids, confs, bbox = net.detect(img, confThreshold=threshold, nmsThreshold=threshold)
+    if len(objects) == 0:
+        objects = coco_names
+
+    object_info = []
+    if len(class_ids) != 0:
+        for classId, confidence, box in zip(class_ids.flatten(), confs.flatten(), bbox):
+            class_name = coco_names[classId - 1]
+
+            if class_name in objects:
+                object_info.append([box, class_name])
+
+                if draw_box:
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(img, class_name.upper(), (box[0] + 10, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    return img, object_info
+
+
+if __name__ == '__main__':
+    objs =[]
+    email_sent = False
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    objs = ['dog', 'teddy bear', 'cat', 'sheep', 'person']
+
+    while True:
+        success, img = cap.read()
+        result, object_info = get_objects(img, objects=objs, threshold=0.6)
+        cv2.imshow("Output", img)
+        print(object_info)
+
+        for i in object_info:
+            if 'teddy bear' in i and not email_sent:
+                send_email()
+                email_sent = True
+
+            if 'teddy bear' not in i and email_sent:
+                email_sent = False
+
+        if cv2.waitKey(40) == 27:
+            break
